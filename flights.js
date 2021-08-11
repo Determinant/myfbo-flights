eec1a7e76abbd8fe3abbed5c511cbd114e84c1f9
Determinant <tederminant@gmail.com>
2021-08-11 12:10:43 -0700

    add a switch const to support both platforms

--- flights.js
+++ flights.js
@@ -12,6 +12,10 @@ axios.defaults.withCredentials = true;
 const { JSDOM } = jsdom;
 
 const userAgent = "Mozilla/5.0 Chrome/89.0.4389.90 Mobile Safari/537.36";
+//const fbo = 'ehfc';
+const fbo = 'advantage'; // change this to your local FBO id of MyFBO/PaperlessFBO
+const is_paperlessfbo = true; // change to false if it is MyFBO
+//const timezone = "America/New_York";
 const timezone = "America/Los_Angeles"; // change this to your location
 
 const encodeForm = (data) => {
@@ -20,11 +24,78 @@ const encodeForm = (data) => {
         .join('&');
 }
 
-async function showFlights() {
+async function showFlightsMyFBO() {
     const cookieJar = new tough.CookieJar();
     const password = fs.readFileSync(__dirname + "/.secret").toString().trim();
     const username = fs.readFileSync(__dirname + "/.username").toString().trim();
-    let ret = await axios.get('https://advantage.paperlessfbo.com', {
+    await axios.get(`https://prod.myfbo.com/b/linkpage_mobile.asp?fbo=${fbo}`, {
+        headers: { 'User-Agent': userAgent },
+        jar: cookieJar,
+        withCredentials: true
+    });
+    await axios
+        .post('https://prod.myfbo.com/b/login_check.asp', encodeForm({
+            'login': 'pda',
+            'email': username,
+            'password': password,
+        }), {
+            headers: {
+                'User-Agent': userAgent,
+                'Referer': `https://prod.myfbo.com/b/linkpage_mobile.asp?fbo=${fbo}`,
+            },
+            jar: cookieJar,
+            withCredentials: true
+        });
+    const ret = await axios.get('https://prod.myfbo.com/ct/rsv_list.asp', {
+        jar: cookieJar,
+        withCredentials: true});
+    const dom = new JSDOM(ret.data);
+    const raw = dom.window.document.querySelector("input[name='msg']");
+    if (!raw) {
+        return '';
+    }
+    raw = raw.value.split('\n');
+    const records = [];
+    const today = moment(new Date());
+    let lessonToday = -1;
+    raw.forEach((r, i) => {
+        let m = r.match(' *\(.*\) beginning \(.*\) until \(.*\)');
+        if (m) {
+            const start = moment.tz(m[2], 'MM/DD/YY HH:mm', timezone);
+            const end = moment.tz(m[3], 'MM/DD/YY HH:mm', timezone);
+            if (start.isSame(today, 'day') ||
+                end.isSame(today, 'day')) {
+                lessonToday = records.length;
+            }
+            records.push({
+                'entity': m[1],
+                'start': start,
+                'end': end
+            });
+        }
+    });
+    let res = '';
+    const fmtTime = m => m.format("HH:mm", timezone);
+    records.forEach(r => {
+        res += `${r.entity}: ${r.start.format("MMM D YYYY")} [${fmtTime(r.start)}-${fmtTime(r.end)}]\n`;
+        if (!r.entity.match('C1[57]2 .*')) {
+            res += '\n';
+        }
+    });
+    if (lessonToday >= 0) {
+        const d = records[lessonToday];
+        res += `!!! There is a lesson today: ${fmtTime(d.start)}-${fmtTime(d.end)}.\n`;
+    } else {
+        res += `No lesson today.\n`;
+    }
+    return res;
+}
+
+async function showFlightsPaperlessFBO() {
+    const cookieJar = new tough.CookieJar();
+    const password = fs.readFileSync(__dirname + "/.secret").toString().trim();
+    const username = fs.readFileSync(__dirname + "/.username").toString().trim();
+    let ret = await axios.get(`https://${fbo}.paperlessfbo.com`, {
         jar: cookieJar,
         withCredentials: true});
     const dom = new JSDOM(ret.data);
@@ -91,6 +162,14 @@ async function showFlights() {
         res += `No lesson today.\n`;
     }
     return res;
+}
+
+async function showFlights() {
+    if (is_paperlessfbo) {
+        return showFlightsPaperlessFBO();
+    } else {
+        return showFlightsMyFBO();
+    }
 }
 
 async function main() {
